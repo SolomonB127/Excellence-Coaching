fd225bed9226bd0ab47350f0ef1ea78cd4eb54b7
SolomonB127 <solomonakinsulire@gmail.com>
2023-09-16 01:39:08 +0100

    Made navBar fully responsive

--- Scripts/Home.js
+++ Scripts/Home.js
@@ -1,17 +1,36 @@
-// Search Dropdown Funtionality
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+// Search Funtionality
+function mysearchFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
   }
+}
+
+// CoursesBtn Funtionality
+function mycourseFunction() {
+  document.getElementById("courseDropdown").classList.toggle("course-show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.course-dropbtn')) {
+    var dropdowns = document.getElementsByClassName("course-dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('course-show')) {
+        openDropdown.classList.remove('course-show');
+      }
+    }
+  }
+}
